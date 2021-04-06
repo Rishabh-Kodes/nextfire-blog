22d0d3330ef323ec84c71f4a10c2f9029d03a783
rishabh-kodes <rishabhkodes@gmail.com>
2021-04-06 23:15:22 +0530

    Custom 404 Page

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -8,6 +8,13 @@ export async function getServerSideProps({ query }) {
 
   const userDoc = await getUserWithUsername(username);
 
+  // If no user, short circuit to 404 page
+  if (!userDoc) {
+    return {
+      notFound: true,
+    };
+  }
+
   let user = null;
   let posts = null;
 
