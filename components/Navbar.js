14ba448887d85cd0b72dc71b1850e79b1d95c1b9
rishabh-kodes <rishabhkodes@gmail.com>
2021-04-14 19:38:19 +0530

    UI Fix: Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,19 @@
 import Link from "next/link";
 import { useContext } from "react";
+import { useRouter } from "next/router";
 import { UserContext } from "@lib/context";
+import { auth } from "@lib/firebase";
 
 // Top Navbar
 export default function Navbar() {
   const { user, username } = useContext(UserContext);
+
+  const router = useRouter();
+
+  const signOut = () => {
+    auth.signOut();
+    router.reload();
+  };
 
   return (
     <nav className="navbar">
@@ -18,18 +27,19 @@ export default function Navbar() {
         {/* User is signed-in and has a username */}
         {username && (
           <>
-            <ul>
-              <li className="push-left">
-                <Link href="/admin">
-                  <button className="btn-blue">Write Posts</button>
-                </Link>
-              </li>
-              <li>
-                <Link href={`${username}`}>
-                  <img src={user?.photoURL} />
-                </Link>
-              </li>
-            </ul>
+            <li className="push-left">
+              <button onClick={signOut}>Sign Out</button>
+            </li>
+            <li>
+              <Link href="/admin">
+                <button className="btn-blue">Write Posts</button>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/${username}`}>
+                <img src={user?.photoURL || "/hacker.png"} />
+              </Link>
+            </li>
           </>
         )}
 
